a2cc0b03d8a76c9259151fcc876a414d89d68c95
Brenn Kucey <bkucey@users.noreply.github.com>
2024-06-06 11:58:55 -0700

    feat: Add git fetch prune

--- .aliases.sh
+++ .aliases.sh
@@ -1,10 +1,14 @@
-echo "Brenn's aliases loading" 
+echo "+++ Brenn's aliases: loading" 
+
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit -m"
 alias gp="git push"
+alias gfp="git fetch --prune"
 alias gitConfigAll="git config --list --show-origin"
 
 # roll back the last git commit, might need to 
 # redo git history 
 alias git-rollback-commit="git reset --soft HEAD~1" 
+
+echo "--- Brenn's aliases: loaded"
