4a5215eeb0ff6bbbd3e3f87dda529c153f88fe33
Brenn Kucey <bkucey@users.noreply.github.com>
2024-06-08 23:11:41 -0700

    feat: shutdown command

--- .aliases.sh
+++ .aliases.sh
@@ -8,3 +8,5 @@ alias gitConfigAll="git config --list --show-origin"
 # roll back the last git commit, might need to 
 # redo git history 
 alias git-rollback-commit="git reset --soft HEAD~1" 
+
+alias shutdown-now="sudo shutdown -h now"
