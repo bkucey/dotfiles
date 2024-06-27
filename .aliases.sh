ec359f9f93436e7b4f46b8cbf597cc16c21e9e58
Brenn Kucey <bkucey@users.noreply.github.com>
2024-06-27 16:15:38 -0700

    feat: Add move and merge

--- .aliases.sh
+++ .aliases.sh
@@ -2,10 +2,14 @@ echo "+++ Brenn's aliases: loading"
 
 alias gs="git status"
 alias ga="git add"
+alias gm="git mv"
 alias gc="git commit -m"
 alias gp="git push"
 alias gfp="git fetch --prune"
 alias gitConfigAll="git config --list --show-origin"
+
+# merge changes from master branch 
+alias git-merge-origin-master="git merge origin/master"
 
 # checkout a new git branch 
 alias git-checkout-new-branch="git checkout -b "
